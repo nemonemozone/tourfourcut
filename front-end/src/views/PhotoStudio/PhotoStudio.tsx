99959142b2e6827a661aa15fb44029de83212951
eeeemune <eeeemune@gmail.com>
2024-07-31 04:04:38 +0000

    base stylesheet and App.tsx(router)

---
+++ front-end/src/views/PhotoStudio/PhotoStudio.tsx
@@ -0,0 +1,13 @@
+function PhotoStudio(): React.ReactElement {
+    return (
+        <div className="page_photo_studio">
+            <div className="comp_top_nav">
+                <div className="container_logo">
+                    gogogo
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default PhotoStudio;
