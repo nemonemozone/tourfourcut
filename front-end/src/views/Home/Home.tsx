cc77107f947332f857ef3e6031be565b578d7f64
yuchan <73932179+oyc0401@users.noreply.github.com>
2024-09-26 08:01:35 +0000

    메인화면 만들었습니다~ tailwind 작동방법 Description에 적어놨어요
    tailwind css 작동시키려면
    터미널에 npx tailwindcss -i ./src/input.css -o ./src/output.css --watch
    쓰세요~

--- front-end/src/views/Home/Home.tsx
+++ front-end/src/views/Home/Home.tsx
@@ -5,22 +5,19 @@ import { useNavigate } from "react-router-dom";
 import Location from "../../types/location";
 import "./Home.scss";
 
-
 export default function Home(): React.ReactElement {
     const [locationListInfo, setLocationListInfo] = useState<Location[]>([]);
     const [myLocation, setMyLocation] = useState<any>(null);
     const [myAddress, setMyAddress] = useState<string | null>(null);
-    const navigate = useNavigate();
-
 
     const fetch_my_location = () => {
         //현재 내 위치와 주소를 불러옵니다.
         const location = "";
-        const address = "인하로 100 인하대학교 하이테크 센터"
+        const address = "인하로 100 인하대학교 하이테크 센터";
         setMyAddress(address);
         setMyLocation(location);
         return location;
-    }
+    };
 
     const fetch_location_list_info = (my_location: string) => {
         //내 위치로부터 3km 반경 내에 있는 관광지들의 정보를 불러옵니다.
@@ -29,26 +26,25 @@ export default function Home(): React.ReactElement {
                 location_ID: "locationID1",
                 title: "익선동 한옥거리",
                 distance: "1.2km",
-                img_url: "http://tong.visitkorea.or.kr/cms/resource/23/2947523_image2_1.jpg"
+                img_url:
+                    "http://tong.visitkorea.or.kr/cms/resource/23/2947523_image2_1.jpg",
             },
             {
                 location_ID: "locationID2",
                 title: "경복궁",
                 distance: "2.5km",
-                img_url: "http://tong.visitkorea.or.kr/cms/resource/23/2947523_image2_1.jpg"
+                img_url:
+                    "http://tong.visitkorea.or.kr/cms/resource/23/2947523_image2_1.jpg",
             },
             {
                 location_ID: "locationID3",
                 title: "북촌 한옥마을",
                 distance: "1.8km",
-                img_url: "http://tong.visitkorea.or.kr/cms/resource/23/2947523_image2_1.jpg"
-            }
+                img_url:
+                    "http://tong.visitkorea.or.kr/cms/resource/23/2947523_image2_1.jpg",
+            },
         ];
         setLocationListInfo(mock_data);
-    };
-
-    const handle_location_clicked = (locationID: string) => {
-        navigate(`/location/${locationID}`);
     };
 
     useEffect(() => {
@@ -59,40 +55,62 @@ export default function Home(): React.ReactElement {
         init();
     }, []);
 
-    return (
-        locationListInfo.length > 0 ? (
-            <div className="page_home">
-                <TopNav />
+    return locationListInfo.length > 0 ? (
+        <div className="page_home">
+            <TopNav />
+            <div className="layout">
                 <div className="comp_my_location">
-                    <div className="wrap_icon_title">
-                        <span className="material-icons">my_location</span>
-                        <p>내 주변 투어네컷</p>
-                    </div>
-                    <p className="font-bold text-2xl pt-2">{myAddress}</p>
-                </div>
-                <div className="comp_location_list">
-                    {locationListInfo.map((_location) => (
-                        <div
-                            className="comp_location"
-                            onClick={() => handle_location_clicked(_location.location_ID)}
-                            key={_location.location_ID}
-                        >
-                            <div className="image_container">
-                                <img alt={_location.title} src={_location.img_url} />
-                            </div>
-                            <div className="wrap_texts">
-                                <div className="wrap_icon_title">
-                                    <span className="material-icons">near_me</span>
-                                    <p className="distance">{_location.distance}</p>
-                                </div>
-                                <p className="location_title">{_location.title}</p>
-                            </div>
+                    <div className="flex flex-row">
+                        <div className="material-icons text-sm text-[#E44D71]">
+                            my_location
                         </div>
+                        <p className="pl-2 text-sm text-[#E44D71]">
+                            내 주변 투어네컷
+                        </p>
+                    </div>
+                    <p className="font-bold text-2xl pt-2 pb-4">{myAddress}</p>
+                </div>
+                <div className="grid grid-cols-2 gap-2">
+                    {locationListInfo.map((_location) => (
+                        <DinnerCard location={_location} />
                     ))}
                 </div>
             </div>
-        ) : (
-            <Loading />
-        )
+        </div>
+    ) : (
+        <Loading />
+    );
+}
+
+function DinnerCard({ location }) {
+    const navigate = useNavigate();
+
+    const handle_location_clicked = (locationID: string) => {
+        navigate(`/location/${locationID}`);
+    };
+
+    return (
+        <>
+            <div
+                className="comp_location"
+                onClick={() => handle_location_clicked(location.location_ID)}
+                key={location.location_ID}
+            >
+                <img
+                    className="h-auto aspect-square object-cover"
+                    alt={location.title}
+                    src={location.img_url}
+                />
+                <div className="pt-2">
+                    <div className="flex flex-row">
+                        <div className="material-icons text-sm text-[#838383]">
+                            near_me
+                        </div>
+                        <p className="distance">{location.distance}</p>
+                    </div>
+                    <p className="location_title">{location.title}</p>
+                </div>
+            </div>
+        </>
     );
 }
--- front-end/src/views/PhotoStudio/components/TopNav.tsx
+++ front-end/src/views/PhotoStudio/components/TopNav.tsx
@@ -7,15 +7,28 @@ export default function TopNav(props: any): React.ReactElement {
     // const history = useHistory();
     const handleLogoBtn = () => {
         navigate("/" + eventID);
-    }
+    };
     return (
-        <div className="comp_top_nav" onClick={handleLogoBtn}>
-            <div className="container_logo">
-                <div className="container">
-                    <img src="/LOGO_Happics.svg" />
-                </div>
-                <p className="logo_ci">tourfourcut</p>
+        <>
+            <div
+                className={
+                    "Comp-app-bar p-2 w-full bg-white flex flex-col justify-center font-bold border-b border-[#D7D7D7]"
+                }
+                onClick={handleLogoBtn}
+            >
+                <img
+                    className="h-8 cursor-pointer"
+                    src="/LOGO_nemozone.svg"
+                ></img>
             </div>
-        </div>
-    )
+        </>
+        // <div className="comp_top_nav" onClick={handleLogoBtn}>
+        //     <div className="container_logo">
+        //         <div className="container">
+        //             <img src="/LOGO_Happics.svg" />
+        //         </div>
+        //         <p className="logo_ci">tourfourcut</p>
+        //     </div>
+        // </div>
+    );
 }
