76c15513e778690d9c369a0ed0a85f8a8274be8e
yuchan <73932179+oyc0401@users.noreply.github.com>
2024-09-26 10:26:27 +0000

    테마 선택 개발 완료.. 근데 페이지네이션 위치 조정 필요함!!

--- front-end/src/views/LocationTheme/components/TopNav.tsx
+++ front-end/src/views/LocationTheme/components/TopNav.tsx
@@ -2,19 +2,28 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 
 export default function TopNav(props: any): React.ReactElement {
-    console.log(props)
+    console.log(props);
 
     return (
-        <div className="comp_top_nav">
-            <div className="container_logo" onClick={props.handle_logo_click}>
-                <div className="container">
-                    <img src="/LOGO_Happics.svg" />
-                </div>
-                <p className="logo_ci">Happics</p>
+        <div className="py-2.5 w-full flex flex-row items-center justify-between font-normal">
+            <div
+                className="pl-6 w-[80px] cursor-pointer"
+                onClick={() => {
+                    window.history.back();
+                }}
+            >
+                <img
+                    className="h-[13px] cursor-pointer"
+                    src={"/left_arrow.svg"}
+                ></img>
             </div>
-            <button onClick={props.handleNxtBtn}>
+            <p className="text-xl text-white">테마선택</p>
+            <p
+                onClick={props.handleNxtBtn}
+                className="text-xl text-[#E44D71] pr-6 w-[80px] text-right cursor-pointer"
+            >
                 다음
-            </button>
+            </p>
         </div>
-    )
+    );
 }
